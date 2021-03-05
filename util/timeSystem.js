74fdad4be1e1ed83e44e0cee5df4b2cc3a8ef8d5
TSP_Panda <chris.barwick16@gmail.com>
2021-03-04 20:55:29 -0800

    Rough implementation of Day/Night Cycle

---
+++ util/timeSystem.js
@@ -0,0 +1,55 @@
+const dataHelper = require('./dataHelper');
+const helper = require('./helper');
+
+const { guildId, announceChannelId } = require('../config.json')
+
+let Client = null;
+
+let day = 1;
+let hour = 0;
+
+function getState() {
+    if (hour == 0) return 'Midnight';
+    if (hour >= 1 && hour < 6) return 'Dawn';
+    if (hour >= 6 && hour < 9) return 'Morning';
+    if (hour >= 9 && hour < 12) return 'Mid-Morning';
+    if (hour >= 12 && hour < 13) return 'Noon';
+    if (hour >= 13 && hour < 16) return 'Afternoon';
+    if (hour >= 16 && hour < 21) return 'Evening';
+    if (hour >= 21) return 'Night';
+}
+
+async function announceDayChange() {
+    let guild = await Client.guilds.resolve(guildId);
+    
+    if (guild) {
+        let channel = await guild.channels.resolve(announceChannelId);
+
+        if (channel) {
+            // return channel.send(`Day ${day} - Hour ${hour} [${getState()}]`);
+        } else {
+            console.log('No channel.');
+        }
+    } else {
+        console.log('No guild.');
+    }
+}
+
+module.exports = {
+    start: async function(client) {
+        Client = client;
+        await announceDayChange();
+        setInterval(async function() {
+            hour = hour + 1
+        
+            if (hour >= 24) {
+                hour = 0;
+                day = day + 1
+            }
+
+            await announceDayChange();
+        }, 1000*60*15);
+    },
+
+    getState: getState
+}
